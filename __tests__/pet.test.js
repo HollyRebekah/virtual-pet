5b2cc25e8907c81383e570d358855c3980adb297
Holly <hollyfanthorpe@gmail.com>
2019-06-17 21:11:24 +0100

    Created grow up method on Pet prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,4 +10,18 @@ describe('constructor', () =>{
 
         expect(pet.name).toEqual('Fido');
     });
+
+    it('has an inital age of 0', () => {
+        const pet = new Pet('Fido');
+        
+        expect(pet.age).toEqual(0);
+    });
+
+    it('increases age by 1', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1)
+    });
 });
