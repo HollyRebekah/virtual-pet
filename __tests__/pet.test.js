8f534214d4119fb703b3045a69312b7f4bfa2364
Holly <hollyfanthorpe@gmail.com>
2019-06-20 19:39:25 +0100

    Added additional functions to growUp method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,27 +1,55 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () =>{
+    let pet
+    beforeEach(() => {
+        pet = new Pet('Fido');
+      });
+
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
 
     it('sets the name property', () => {
-        const pet = new Pet('Fido');
-
         expect(pet.name).toEqual('Fido');
     });
 
     it('has an inital age of 0', () => {
-        const pet = new Pet('Fido');
-        
         expect(pet.age).toEqual(0);
     });
 
-    it('increases age by 1', () => {
-        const pet = new Pet('Fido');
+    it ('has an initial hunger of 0', () => {
+        expect(pet.hunger).toEqual(0);
+    });
 
+    it('has an initial fitness of 10', () => {
+        expect(pet.fitness).toEqual(10)
+    });
+});
+
+describe('growUp function', () =>{
+    let pet
+    beforeEach(() => {
+        pet = new Pet('Fido');
+    });
+
+    it('growUp increases age by 1', () => {
         pet.growUp();
 
         expect(pet.age).toEqual(1)
     });
+
+    it('growUp increases hunger by 5', () =>{
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it('growUp decreases fitness by 3', () =>{
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
+
 });
+      
